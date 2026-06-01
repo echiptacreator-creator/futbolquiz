4b4351bdaf55421d8341d1b7a384af5dbbb1f686
Maladesbola <echiptacreator@gmail.com>
2026-06-02 02:43:10 +0500

    Add deletion of webhook with pending updates

--- bot.py
+++ bot.py
@@ -647,8 +647,11 @@ async def main():
 
     await create_tables()
 
-    await dp.start_polling(bot)
+    await bot.delete_webhook(
+        drop_pending_updates=True
+    )
 
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
